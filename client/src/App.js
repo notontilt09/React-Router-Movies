ba9218c86c56dda72536b2d1a62587ab0b89a0b2
Daniel Weinman <namniewd@gmail.com>
2019-01-22 16:34:11 -0500

    converted Route to Movie component to use render function

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,15 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path='/' component={MovieList} />
-        <Route path='/movies/:id' component={Movie} />
+        <Route 
+          exact 
+          path='/' 
+          component={MovieList} 
+        />
+        <Route 
+          path='/movies/:id' 
+          render={props => <Movie {...props} addToSavedList={this.addToSavedList} /> } 
+        />
       </div>
     );
   }
